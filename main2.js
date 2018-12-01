dc3a5d544eaa26c1254cf30edebf9c0af08795da
PJC-1 <jumpzine@gmail.com>
2018-11-30 23:53:08 -0800

    added addAll() function with es5 solution

---
+++ main2.js
@@ -0,0 +1,24 @@
+// CHALLENGE 1: ADD ALL NUMBERS
+// Return a sum of all parameters entered regardless of the amount of numbers
+// ex. addAll(2,5,6,7) === 20
+
+// Solution - ES5 arguments & for loop
+// function addAll() {
+//   var args = Array.prototype.slice.call(arguments);
+//   total = 0;
+//   for(var i = 0; i < args.length; i++) {
+//     total += args[i];
+//   }
+//   return total;
+// }
+
+// Solution 2 - ...rest & reduce/forEach
+function addAll(...numbers) {
+  let total = 0;
+  numbers.forEach((num) => {
+    total += num;
+  });
+  return total;
+}
+
+console.log(addAll(2,5,6,7));
