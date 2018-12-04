be76145a4ae30eb03b650a1ef44c2c6b873f6275
PJC-1 <jumpzine@gmail.com>
2018-12-03 21:22:16 -0800

    added an example for map

--- higher_order_functions.js
+++ higher_order_functions.js
@@ -47,15 +47,15 @@ var reformattedArray = kvArray.map(function(obj) {
 
 // solution without Higher-Order Function.
 
-const arr1 = [1, 2, 3];
-const arr2 = [];
-
-for(let i = 0; i < arr1.length; i++) {
-  arr2.push(arr1[i] * 2);
-}
-
-// prints [2, 4, 6]
-console.log(arr2);
+// const arr1 = [1, 2, 3];
+// const arr2 = [];
+//
+// for(let i = 0; i < arr1.length; i++) {
+//   arr2.push(arr1[i] * 2);
+// }
+//
+// // prints [2, 4, 6]
+// console.log(arr2);
 
 // solution with Higher-Order Function map()
 
@@ -72,6 +72,25 @@ const arr1 = [1, 2, 3];
 const arr2 = arr1.map(item => item * 2);
 console.log(arr2);
 
+// map example 3
+// Let's say we have an array containing the birth year of different persons and
+// we want to create an array that contains their ages.
+
+// // solution without Higher-Order Functions
+// const birthYear = [1975, 1997, 2002, 1995, 1985];
+// const ages = [];
+// for(let i = 0; i < birthYear.length; i++) {
+//   let age = 2018 - birthYear[i];
+//   ages.push(age);
+// }
+// //prints [43, 21, 16, 23, 33]
+// console.log(ages);
+
+// solution using map()
+const birthYear = [1975, 1997, 2002, 1995, 1985];
+const ages = birthYear.map(age => 2018 - age);
+//prints [43, 21, 16, 23, 33]
+console.log(ages);
 
 // reduce
 
