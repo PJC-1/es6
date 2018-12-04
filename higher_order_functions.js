9258f769240664a2ef06d2ac2f743ca6c72f2848
PJC-1 <jumpzine@gmail.com>
2018-12-03 22:15:22 -0800

    added notes and example on filter

--- higher_order_functions.js
+++ higher_order_functions.js
@@ -14,6 +14,8 @@ a.forEach(function(element) {
 
 // filter
 
+// filter example 1
+
 function isBigEnough(value) {
   return value >= 10;
 }
@@ -22,6 +24,45 @@ var filtered = [12, 5, 8, 130, 44].filter(isBigEnough);
 
 // output:
 // filtered is [12, 130, 44]
+
+// filter example 1
+// Let's say we have an array which contains objects with name and age properties.
+// We want to create an array that contains only the persons with full age (age greater than or equal to 18)
+
+// solution without higher-order function
+// const persons = [
+//   { name: 'Peter', age: 16 },
+//   { name: 'Mark', age: 18 },
+//   { name: 'John', age: 27 },
+//   { name: 'Jane', age: 14 },
+//   { name: 'Tony', age: 24 }
+// ];
+//
+// const fullAge = [];
+//
+// for(let i = 0; i < persons.length; i++) {
+//   if(persons[i].age >= 18) {
+//     fullAge.push(person[i]);
+//   }
+// }
+//
+// // outputs:
+// // [ { name: 'Mark', age: 18 },
+// //   { name: 'John', age: 27 },
+// //   { name: 'Tony', age: 24 } ]
+// console.log(fullAge);
+
+// solution with higher-order function filter
+const persons = [
+  { name: 'Peter', age: 16 },
+  { name: 'Mark', age: 18 },
+  { name: 'John', age: 27 },
+  { name: 'Jane', age: 14 },
+  { name: 'Tony', age: 24 }
+];
+
+const fullAge = persons.filter(person => person.age >= 18);
+console.log(fullAge);
 
 
 // map example 1
