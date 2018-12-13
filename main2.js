d16b435c0ac94170638e378fe8769f08db6679f7
PJC-1 <jumpzine@gmail.com>
2018-12-12 23:03:59 -0800

    largestOfFour nested for loop solution

--- main2.js
+++ main2.js
@@ -213,3 +213,26 @@ function titleCase(str) {
 
 // returns "Hello World"
 titleCase("hEllo wOrld");
+
+// RETURN LARGEST NUMBERS IN ARRAYS
+// Return an array consisting of the largest number from each provided sub-array.
+// For simplicity, the provided array will contain exactly 4 sub-arrays.
+
+function largestOfFour(arr) {
+  var maxes = [];
+
+for(var i = 0; i < arr.length; i++) {
+  var tempMax = arr[i][0];
+    for(var j = 0; j < arr[i].length; j++) {
+      var currentElement = arr[i][j];
+      if(currentElement >= tempMax) {
+        tempMax = currentElement;
+      }
+    }
+    maxes.push(tempMax);
+  }
+  return maxes;
+}
+
+// outputs [5, 27, 39, 1001]
+console.log(largestOfFour([[4,5,1,3],[13,27,18,26],[32,35,37,39],[1000,1001,857,1]]));
