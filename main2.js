17dea8a0e9dc6dfbf4783f53ef9f55fdf886078f
PJC-1 <jumpzine@gmail.com>
2018-12-15 22:43:54 -0800

    truncateString function

--- main2.js
+++ main2.js
@@ -320,3 +320,22 @@ function repeatStringNumTimes(str, num) {
 }
 
 repeatStringNumTimes("abc", 3);
+
+
+// TRUNCATE A STRING
+// Truncate a string (first argument) if it is longer than the given maximum string length (second argument). Return the truncated string with a ... ending.
+// Note that instering the three dots to the end will add to the string length.
+// If the given maximum string length num is less than or equal to 3, then the addition of the three dots does not add to the string length in determining the truncated string.
+
+function truncateString(str, num) {
+  if(num >= str.length) return str;
+  if(num <= 3) return str.slice(0, num) + "...";
+  return str.slice(0, num - 3) + "...";
+}
+
+// output "A-tisket..."
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", 11));
+// output the whole string argument
+console.log(truncateString("A-tisket a-tasket A green and yellow basket", "A-tisket a-tasket A green and yellow basket".length ));
+// output "hel..."
+console.log(truncateString("hello world", 3));
