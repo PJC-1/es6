fe72ccff45faa762e65ab017f75743d3b6596aa4
PJC-1 <jumpzine@gmail.com>
2018-12-19 23:27:17 -0800

    caesars cipher solution

--- main2.js
+++ main2.js
@@ -482,3 +482,28 @@ console.log(bouncer([7, "ate", "", false, 9]));
  }
 
  console.log(getIndexToIns([40, 60], 50));
+
+// CAESARS CIPHER
+// One of the simplest and most widely known ciphers is a Caesar cipher, also known
+// as a shift cipher.
+// In a shit cipher the meanings of the letters are shifted by some set amount.
+// A common modern use is the ROT13 cipher, where the values of the letters are shifted by 13 places. Thus "A" <-> "N", "B" <-> "O" and so on.
+// Write a function which takes a ROT13 encoded string as input and returns a decoded string.
+// All letters will be upperase. Do not transform any non-alphabetic characters (i.e. spaces, punctuation), but do pass them on.
+function rot13(str) {
+  var solved = "";
+  for(var i = 0; i < str.length; i++) {
+    var asciiNum = str[i].charCodeAt();
+    if(asciiNum >= 65 && asciiNum <= 77) {
+      solved += String.fromCharCode(asciiNum + 13);
+    } else if(asciiNum >= 78 && asciiNum <= 90) {
+      solved += String.fromCharCode(asciiNum - 13);
+    } else {
+      solved += str[i];
+    }
+  }
+  return solved;
+}
+
+// [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z]
+console.log(rot13("URYYB JBEYQ"));
