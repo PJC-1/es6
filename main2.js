1982fa2c5c254e934a56a8b9158246932360b071
PJC-1 <jumpzine@gmail.com>
2018-12-21 10:52:08 -0800

    diffArray function for loop solution

--- main2.js
+++ main2.js
@@ -526,3 +526,27 @@ function sumAll(arr) {
 
 // returns 6
 console.log(sumAll([1, 4]));
+
+// DIFF TWO ARRAYS
+// Compare two arrays and return a new array with any items only found in one of the two given arrays, but not both.
+// In other words, return the symmetric difference of the two arrays.
+function diffArray(arr1, arr2) {
+  var result = [];
+
+  for(var i = 0; i < arr1.length; i++) {
+    if(arr2.indexOf(arr1[i]) === -1) {
+      result.push(arr1[i]);
+    }
+  }
+
+  for(var j = 0; j < arr2.length; j++) {
+    if(arr1.indexOf(arr2[j]) === -1) {
+      result.push(arr2[j]);
+    }
+  }
+
+  return result;
+}
+
+// return [7, 4]
+console.log(diffArray([1, 2, 3, 5, 7], [1, 2, 3, 4, 5]));
