772b86e72921999a9329ab58d44e3cfd641ff2ac
PJC-1 <jumpzine@gmail.com>
2018-12-22 17:07:05 -0800

    myReplace split array solution

--- main2.js
+++ main2.js
@@ -633,4 +633,19 @@ function myReplace(str, before, after) {
   return str.replace(before, after);
 }
 
+function myReplace(str, before, after) {
+  var words = str.split(" ");
+  if(before[0] === before[0].toUpperCase()) {
+    after = after[0].toUpperCase() + after.slice(1);
+  }
+
+  for(var i = 0; i < words.length; i++) {
+    if(words[i] === before) {
+      words[i] = after;
+    }
+  }
+
+  return words.join(" ");
+}
+
 console.log(myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped"));
