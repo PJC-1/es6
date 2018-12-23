649465a47e370120b046a58151cd665396ae847a
PJC-1 <jumpzine@gmail.com>
2018-12-22 16:01:14 -0800

    myReplace function String.replace solution

--- main2.js
+++ main2.js
@@ -618,3 +618,19 @@ function whatIsInAName(collection, source) {
 
 // returns [ { first: 'Tybalt', last: 'Capulet' } ]
 console.log(whatIsInAName([{ first: "Romeo", last: "Montague" }, { first: "Mercutio", last: null }, {first: "Tybalt", last: "Capulet" }], { last: "Capulet" }));
+
+// SEARCH AND REPLACE
+// Perform search and replace on the sentence using the arguments provided and return the new sentence
+// First argument is the sentence to perform the search and replace on.
+// Second argument is the word that you will be replacing (before).
+// Third argument is what you will be replacing the second argument with (after).
+// NOTE: Preserve the case of the original word when you are replacing it. For example if you mean to replace the word "Book" with the word "dog", it should be replace as "Dog".
+function myReplace(str, before, after) {
+  if(before[0] === before[0].toUpperCase()) {
+    after = after[0].toUpperCase() + after.slice(1);
+  }
+
+  return str.replace(before, after);
+}
+
+console.log(myReplace("A quick brown fox Jumped over the lazy dog", "Jumped", "leaped"));
