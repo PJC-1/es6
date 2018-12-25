6603f765516c06f9359d58e8cd1a88c5715dc8fb
PJC-1 <jumpzine@gmail.com>
2018-12-25 00:12:08 -0800

    To Lower Case function solution without built-in method toLowerCase()

--- main2.js
+++ main2.js
@@ -757,3 +757,38 @@ var numUniqueEmails = function(emails) {
 
 // returns 2
 console.log(numUniqueEmails(["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]));
+
+
+// TO LOWER CASE
+// Implement function ToLowerCase() that has a string parameter str, and returns the same string in lowercase.
+//
+// Example 1:
+//
+// Input: "Hello"
+// Output: "hello"
+// Example 2:
+//
+// Input: "here"
+// Output: "here"
+// Example 3:
+//
+// Input: "LOVELY"
+// Output: "lovely"
+
+// solution wihout built-in toLowerCase() method
+var toLowerCase = function(str) {
+  var result = "";
+
+  for(var i = 0; i < str.length; i++) {
+    if(str.charCodeAt(i) >= 65 && str.charCodeAt(i) <= 90) {
+      result = result + String.fromCharCode(str.charCodeAt(i) + 32);
+    } else {
+      result = result + str[i];
+    }
+  }
+
+  return result;
+};
+
+// returns "hello world"
+console.log(toLowerCase("HELLO WORLD"));
