e4c5f68ad789cb9429d35901038ab28296c600b8
PJC-1 <jumpzine@gmail.com>
2019-01-23 16:42:07 -0800

    longest word in dictionary comments

--- longest_word_in_dictionary.js
+++ longest_word_in_dictionary.js
@@ -15,30 +15,43 @@
 // Both "apply" and "apple" can be built from other words in the dictionary. However, "apple" is lexicographically smaller than "apply".
 
 var longestWord = function(words) {
-
+    // sort the passed in words array
     words.sort()
 
+    // creating a set of the words array using the built in javascript Set data structure
     const wordSet = new Set(words)
-
+    // storage variable
     let result = ''
 
+    // iterate over the words array
     for (let word of words) {
+        // boolean value to use for logic check
         let isValid = true;
+        // an empty string that will store/build the word that will be used to check against the words Set
         let key = '';
 
+        // nested loop to iterate over the word's characters
         for (let i = 0; i < word.length - 1; i++) {
+            // add the current iterated character to the key storage string
             key += word[i];
+            // check if the key string doesn't exists in the words Set
             if (!wordSet.has(key)) {
+                // if true then set the boolean variable to false
                 isValid = false;
+                // and break out of the for-loop
                 break;
             }
         }
 
+        // check if boolean variable is true,
+        // and the current iterated word's length is larger than the result string
         if (isValid && word.length > result.length) {
+            // if condition is true, then set result to the current iterated word
             result = word;
         }
     }
 
+    // after the initial loop has completed, return result
     return result;
 };
 
