c91a2b9c1663616da9eaac1dc70337295e202c53
PJC-1 <jumpzine@gmail.com>
2019-02-05 16:58:37 -0800

    corrected keys and rooms solution to handle edge case where keys are found out of order

--- keys_and_rooms.js
+++ keys_and_rooms.js
@@ -24,28 +24,45 @@
 // Explanation: We can't enter the room with number 2.
 
 var canVisitAllRooms = function(rooms) {
-  // object for storage, start out with key 0 by default
-  let obj = {
-    0: true
-  };
-  console.log(rooms);
-  // initial loop to iterate through the rooms
-  for(let i = 0; i < rooms.length; i++) {
-    // check if the storage object contains the key
-    if(obj.hasOwnProperty(i)) {
-      // loop through the nested array, which are the available keys
-      for(let j = 0; j < rooms[i].length; j++) {
-        // add the found key to the storage object
-        obj[rooms[i][j]] = true;
+    // object for storage, start out with key 0 by default
+    let obj = {
+      0: true
+    };
+    // object to keep track of rooms you were unable to access
+    let denied = {};
+
+    // initial loop to iterate through the rooms
+    for(let i = 0; i < rooms.length; i++) {
+      // check if you have the key to this room
+      if(obj.hasOwnProperty(i)) {
+        console.log(`Access granted, you have the key for room ${i}`);
+        // loop through the nested array, which are the available keys
+        for(let j = 0; j < rooms[i].length; j++) {
+          // add the found key to the storage object
+          obj[rooms[i][j]] = true;
+        }
+      // if the key of the iterated room does not exists in the storage object,
+      // then add the room number to the denied storage object
+      } else {
+        console.error(`Access denied, you do NOT have the key for room ${i}`);
+        // add room number to the denied object
+        denied[i] = true;
       }
-    // if the key of the iterated room does not exists in the storage object,
-    // then return false
-    } else {
-      return false;
     }
-  }
-  // if you exit the initial loop without return false at any interation, then you have accessed every door, return true
-  return true;
+
+    // now that you have obtained all possible keys, check all previously denied rooms to see if you have picked up the key
+    for(let room in denied) {
+      // check if you have the key in the storage object
+      if(obj.hasOwnProperty(room)) {
+        console.log(`Access granted, now you may enter room ${room}`);
+      } else {
+        // if you still are denied access this second time around, it means you have not picked up the key for this room, return false
+        return false;
+      }
+    }
+
+    // if you exit the initial loop and secondary loop without return false at any time, then you have accessed every door, return true
+    return true;
 };
 
 // returns true
@@ -53,3 +70,6 @@ canVisitAllRooms([[1],[2],[3],[]]);
 
 // returns false
 canVisitAllRooms([[1,3],[3,0,1],[2],[0]]);
+
+// returns true
+canVisitAllRooms([[2],[],[1]]);
