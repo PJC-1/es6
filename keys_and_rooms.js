a064f03f40f67878c120c90b3899e412a151f910
PJC-1 <jumpzine@gmail.com>
2019-02-06 21:51:28 -0800

    keys and rooms solution that passes all test cases

--- keys_and_rooms.js
+++ keys_and_rooms.js
@@ -23,46 +23,32 @@
 // Output: false
 // Explanation: We can't enter the room with number 2.
 
-var canVisitAllRooms = function(rooms) {
-    // object for storage, start out with key 0 by default
-    let obj = {
-      0: true
-    };
-    // object to keep track of rooms you were unable to access
-    let denied = {};
+const canVisitAllRooms = function(rooms) {
+    // cache the default room you have access to
+    const zero = rooms[0];
+    // the stack is an array that will store room we need to visit
+    let stack = [zero];
+    // the visited array will keep track of the rooms we have visited
+    let visited = [];
+    // set the 0th room as visited
+    visited[0] = true;
 
-    // initial loop to iterate through the rooms
-    for(let i = 0; i < rooms.length; i++) {
-      // check if you have the key to this room
-      if(obj.hasOwnProperty(i)) {
-        console.log(`Access granted, you have the key for room ${i}`);
-        // loop through the nested array, which are the available keys
-        for(let j = 0; j < rooms[i].length; j++) {
-          // add the found key to the storage object
-          obj[rooms[i][j]] = true;
-        }
-      // if the key of the iterated room does not exists in the storage object,
-      // then add the room number to the denied storage object
-      } else {
-        console.error(`Access denied, you do NOT have the key for room ${i}`);
-        // add room number to the denied object
-        denied[i] = true;
-      }
+    // while loop until their are no more rooms in the stack
+    while(stack.length > 0) {
+        // the current room will be the last item in the stack array
+        let curr = stack.pop();
+        // use foreach to iterate over the current array
+        curr.forEach(el => {
+            // if the currently iterated item is not in the visited array, then push the value to the stack
+            if (visited[el] !== true) stack.push(rooms[el]);
+            // also set it as visited
+            visited[el] = true;
+        })
     }
-
-    // now that you have obtained all possible keys, check all previously denied rooms to see if you have picked up the key
-    for(let room in denied) {
-      // check if you have the key in the storage object
-      if(obj.hasOwnProperty(room)) {
-        console.log(`Access granted, now you may enter room ${room}`);
-      } else {
-        // if you still are denied access this second time around, it means you have not picked up the key for this room, return false
-        return false;
-      }
-    }
-
-    // if you exit the initial loop and secondary loop without return false at any time, then you have accessed every door, return true
-    return true;
+    // use the filter function to return an array of elements that are NOT undefined
+    visited = visited.filter(room => room != undefined);
+    // if the length of the visited array is qual to the rooms array, then we have visted all rooms, return true, else return false
+    return visited.length === rooms.length;
 };
 
 // returns true
@@ -73,3 +59,6 @@ canVisitAllRooms([[1,3],[3,0,1],[2],[0]]);
 
 // returns true
 canVisitAllRooms([[2],[],[1]]);
+
+// returns true
+canVisitAllRooms([[6,7,8],[5,4,9],[],[8],[4],[],[1,9,2,3],[7],[6,5],[2,3,1]]);
